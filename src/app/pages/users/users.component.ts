02fa8b615a25854f5ef05ef75b94d62bdcf25285
Guillaume Crochemore <guillaume.crochemore@gmail.com>
2017-06-09 13:54:22 +0200

    feat(fut): improve data

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -10,13 +10,13 @@ import {Router} from "@angular/router";
 
 export class UsersComponent {
 
-  players: [{}] = [{name: 'Souleymane', rating: 75, picture: 'lacazette.png', company: 'trsb.png'},
-                   {name: 'Mederic', rating: 80, picture: 'ronaldo.png', company: 'trsb.png'},
-                   {name: 'Guillaume', rating: 80, picture: 'forlan.png', company: 'trsb.png'},
+  players: [{}] = [{name: 'Souleymane', rating: 81, picture: 'lacazette.png', company: 'trsb.png'},
+                   {name: 'Mederic', rating: 85, picture: 'ronaldo.png', company: 'trsb.png'},
+                   {name: 'Guillaume', rating: 84, picture: 'forlan.png', company: 'trsb.png'},
                    {name: 'Arthur', rating: 65, picture: 'mathieu.png', company: 'trsb.png'},
                    {name: 'Etienne', rating: 76, picture: 'rooney.png', company: 'trsb.png'},
                    {name: 'Gwen', rating: 78, picture: 'gillet.png', company: 'trsb.png'},
-                   {name: 'Joueur 2', rating: 60, picture: 'messi.png', company: 'trsb.png'},
+                   {name: 'Clement', rating: 80, picture: 'bernardo-silva.png', company: 'trsb.png'},
                    {name: 'Joueur 3', rating: 80, picture: 'messi.png', company: 'trsb.png'},
                    {name: 'Joueur 4', rating: 65, picture: 'messi.png', company: 'trsb.png'},
                    {name: 'Joueur 5', rating: 76, picture: 'messi.png', company: 'trsb.png'},
